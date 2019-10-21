635bdce6a8fd7ef736140e80cefabd77e82f5793
sitRyo <al15030@shibaura-it.ac.jp>
2019-10-22 01:29:05 +0900

    [Fixed] Fixed Demo Scene and Scripts, Material.

--- Assets/DungeonTemplateLibrary/Demo/Scripts/GenerateDungeon.cs
+++ Assets/DungeonTemplateLibrary/Demo/Scripts/GenerateDungeon.cs
@@ -12,22 +12,43 @@
     file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 #######################################################################################*/
 
+using System;
 using UnityEngine;
 using DTL.Shape;
 
 public class GenerateDungeon : MonoBehaviour { 
 
-    private SimpleRogueLike simpleRougeLike = new SimpleRogueLike(1, 2, 3, 4, 5, 2, 5, 2);
+    private SimpleRogueLike simpleRougeLike = new SimpleRogueLike(1, 2, 4, 5, 5, 2, 5, 2);
     private int width = 48;
     private int height = 36;
 
+    public GameObject Floor;
+    public GameObject Wall;
 
-    void Start() {
+    private void Start() {
         int[,] matrix = new int[height, width];
         simpleRougeLike.Draw(matrix);
+        ConsoleDraw(matrix);
+        DungeonInstantiate(matrix);
+    }
+
+    private void DungeonInstantiate(int[,] matrix) {
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < width; ++j) {
+                if (matrix[i, j] == 0) {
+                    Instantiate(Wall, new Vector3(j, 0, i), Quaternion.identity);
+                }
+                else {
+                    Instantiate(Floor, new Vector3(j, 0, i), Quaternion.identity);
+                }
+            }
+        }
+    }
+
+    private void ConsoleDraw(int[,] matrix) {
         string s = "\n";
         for (int i = 0; i < height; ++i) {
-            for (int j = 0; j < width; ++j) 
+            for (int j = 0; j < width; ++j)
                 s += matrix[i, j].ToString();
             s += "\n";
         }

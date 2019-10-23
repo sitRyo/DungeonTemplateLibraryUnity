b9b5bcf5bae5c88a46e462bc3ce3d202b82ea065
sitRyo <al15030@shibaura-it.ac.jp>
2019-10-23 22:05:17 +0900

    [Refactoring] Refactoring RogueLikeList script

--- Assets/DungeonTemplateLibrary/Scripts/Base/RogueLikeList.cs
+++ Assets/DungeonTemplateLibrary/Scripts/Base/RogueLikeList.cs
@@ -1,6 +1,19 @@
-﻿using System;
+﻿/*#######################################################################################
+    Copyright (c) 2017-2019 Kasugaccho
+    Copyright (c) 2018-2019 As Project
+    https://github.com/Kasugaccho/DungeonTemplateLibrary
+    wanotaitei@gmail.com
+
+    DungeonTemplateLibraryUnity
+    https://github.com/sitRyo/DungeonTemplateLibraryUnity
+    seriru.rcvmailer@gmail.com
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################*/
+
+using System;
 using System.Runtime.InteropServices.WindowsRuntime;
-using NUnit.Framework;
 
 namespace DTL.Base {
     public class RogueLikeList {
@@ -44,8 +57,6 @@ namespace DTL.Base {
             this.wayId = wayId;
         }
 
-        public RogueLikeList DefaultRogueLikeList() {
-            return new RogueLikeList(0, 1, 2, 3, 4);
-        }
+        public RogueLikeList DefaultRogueLikeList() =>　new RogueLikeList(0, 1, 2, 3, 4);
     }
 }

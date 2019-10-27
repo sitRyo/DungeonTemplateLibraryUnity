c942f7eacc4443dfd600bcdb013026f001799581
sitRyo <al15030@shibaura-it.ac.jp>
2019-10-28 03:28:19 +0900

    [Change] Change and validate Unity Version.

---
+++ Assets/DungeonTemplateLibrary/Scripts/Range/RectBaseRogueLike.cs
@@ -0,0 +1,192 @@
+﻿/*#######################################################################################
+    Copyright (c) 2017-2019 Kasugaccho
+    Copyright (c) 2018-2019 As Project
+    https://github.com/Kasugaccho/DungeonTemplateLibrary
+    wanotaitei@gmail.com
+
+    DungeonTemplateLibraryUnity
+    https://github.com/sitRyo/DungeonTemplateLibraryUnity
+    seriru.rcvmailer@gmail.com
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################*/
+
+using System;
+using DTL.Base;
+using DTL.Shape;
+using DTL.Util;
+using DTL.Interfaces;
+using MatrixRange = DTL.Base.Coordinate2DimensionalAndLength2Dimensional;
+
+namespace DTL.Range {
+
+    /*#######################################################################################
+        [概要] "dtl名前空間"とは"DungeonTemplateLibrary"の全ての機能が含まれる名前空間である。
+        [Summary] The "dtl" is a namespace that contains all the functions of "DungeonTemplateLibrary".
+    #######################################################################################*/
+
+    public class RectBaseRogueLike<TDerived> : BasicRect<RectBaseRogueLike<TDerived>> where TDerived : RectBaseRogueLike<TDerived> {
+        public RogueLikeList rogueLikeList { get; protected set; }
+        public uint maxWay { get; protected set; } = 20;
+        protected MatrixRange roomRange = new MatrixRange(3, 3, 3, 3);
+        protected MatrixRange wayRange = new MatrixRange(3, 3, 12, 12);
+
+        /* Get Member Value */
+
+        public int outSideWall {
+            get => this.rogueLikeList.outsideWallId;
+        }
+
+        public int insideWall {
+            get => this.rogueLikeList.insideWallId;
+        }
+
+        public int room {
+            get => this.rogueLikeList.roomId;
+        }
+
+        public int entrance {
+            get => this.rogueLikeList.entranceId;
+        }
+
+        public int way {
+            get => this.rogueLikeList.wayId;
+        }
+
+        public int wall {
+            get => this.rogueLikeList.outsideWallId;
+        }
+
+        /* Clear */
+        public TDerived ClearOutsideWall() {
+            this.rogueLikeList.outsideWallId = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearInsideWall() {
+            this.rogueLikeList.insideWallId = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearRoom() {
+            this.rogueLikeList.roomId = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearEntrance() {
+            this.rogueLikeList.entranceId = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearWall() {
+            ClearInsideWall();
+            ClearOutsideWall();
+            return (TDerived)this;
+        }
+
+        public TDerived ClearMaxWay() {
+            this.maxWay = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearValue() {
+            this.rogueLikeList = new RogueLikeList();
+            ClearMaxWay();
+            return (TDerived)this;
+        }
+
+        /* Constructors */
+
+        public RectBaseRogueLike() { } // = default();
+
+        public RectBaseRogueLike(MatrixRange matrixRange) : base(matrixRange) { }
+
+        public RectBaseRogueLike(uint startX, uint startY, uint width, uint height) : base(startX, startY, width, height) { }
+
+        public RectBaseRogueLike(RogueLikeList drawValue) {
+            this.rogueLikeList = drawValue;
+        }
+
+        public RectBaseRogueLike(RogueLikeList drawValue, uint maxWay) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+        }
+
+        public RectBaseRogueLike(RogueLikeList drawValue, uint maxWay, MatrixRange roomRange) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+        }
+
+        public RectBaseRogueLike(RogueLikeList drawValue, uint maxWay, MatrixRange roomRange, MatrixRange wayRange) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+            this.wayRange = wayRange;
+        }
+
+        public RectBaseRogueLike(int outsideWallId, int insideWallId, int roomId, int entranceId, int wayId) {
+            this.rogueLikeList = new RogueLikeList(outsideWallId, insideWallId, roomId, entranceId, wayId);
+        }
+
+        public RectBaseRogueLike(int outsideWallId, int insideWallId, int roomId, int entranceId, int wayId, uint maxWay) {
+            this.rogueLikeList = new RogueLikeList(outsideWallId, insideWallId, roomId, entranceId, wayId);
+            this.maxWay = maxWay;
+        }
+
+
+        public RectBaseRogueLike(int outsideWallId, int insideWallId, int roomId, int entranceId, int wayId, uint maxWay, MatrixRange roomRange) {
+            this.rogueLikeList = new RogueLikeList(outsideWallId, insideWallId, roomId, entranceId, wayId);
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+        }
+
+        public RectBaseRogueLike(int outsideWallId, int insideWallId, int roomId, int entranceId, int wayId, uint maxWay, MatrixRange roomRange, MatrixRange wayRange) {
+            this.rogueLikeList = new RogueLikeList(outsideWallId, insideWallId, roomId, entranceId, wayId);
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+            this.wayRange = wayRange;
+        }
+
+        public RectBaseRogueLike(MatrixRange matrixRange, RogueLikeList drawValue, uint maxWay) : base(matrixRange) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+        }
+
+        public RectBaseRogueLike(MatrixRange matrixRange, RogueLikeList drawValue, uint maxWay, MatrixRange roomRange) : base(matrixRange) {
+            this.rogueLikeList = drawValue;
+            this.roomRange = roomRange;
+            this.maxWay = maxWay;
+        }
+
+        public RectBaseRogueLike(MatrixRange matrixRange, RogueLikeList drawValue, uint maxWay, MatrixRange roomRange, MatrixRange wayRange) : base(matrixRange) {
+            this.rogueLikeList = drawValue;
+            this.roomRange = roomRange;
+            this.wayRange = wayRange;
+            this.maxWay = maxWay;
+        }
+
+        public RectBaseRogueLike(uint startX, uint startY, uint width, uint height, RogueLikeList drawValue,
+            uint maxWay) : base(startX, startY, width, height) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+        }
+
+        public RectBaseRogueLike(uint startX, uint startY, uint width, uint height, RogueLikeList drawValue,
+            uint maxWay, MatrixRange roomRange) : base(startX, startY, width, height) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+        }
+
+        public RectBaseRogueLike(uint startX, uint startY, uint width, uint height, RogueLikeList drawValue,
+            uint maxWay, MatrixRange roomRange, MatrixRange wayRange) : base(startX, startY, width, height) {
+            this.rogueLikeList = drawValue;
+            this.maxWay = maxWay;
+            this.roomRange = roomRange;
+            this.wayRange = wayRange;
+        }
+    }
+
+}
---
+++ Assets/DungeonTemplateLibrary/Scripts/Range/RectBaseSimpleRogueLike.cs
@@ -0,0 +1,153 @@
+﻿/*#######################################################################################
+    Copyright (c) 2017-2019 Kasugaccho
+    Copyright (c) 2018-2019 As Project
+    https://github.com/Kasugaccho/DungeonTemplateLibrary
+    wanotaitei@gmail.com
+
+    DungeonTemplateLibraryUnity
+    https://github.com/sitRyo/DungeonTemplateLibraryUnity
+    seriru.rcvmailer@gmail.com
+
+    Distributed under the Boost Software License, Version 1.0. (See accompanying
+    file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+#######################################################################################*/
+
+using System;
+using UnityEngine;
+using MatrixRange = DTL.Base.Coordinate2DimensionalAndLength2Dimensional;
+
+/*
+ * Range機能(配列の特定の範囲を描画する機能)は未実装.
+ * 将来的に実装することを見据えて名前空間は本家DTLに倣うことにする.
+ */
+
+namespace DTL.Range {
+    /*#######################################################################################
+        [概要] "dtl名前空間"とは"DungeonTemplateLibrary"の全ての機能が含まれる名前空間である。
+        [Summary] The "dtl" is a namespace that contains all the functions of "DungeonTemplateLibrary".
+    #######################################################################################*/
+
+    public class RectBaseSimpleRogueLike<TDerived> where TDerived : RectBaseSimpleRogueLike<TDerived> {
+        public uint startX { get; set; }
+        public uint startY { get; set; }
+        public uint width { get; set; }
+        public uint height { get; set; }
+
+        public int roomValue { get; set; }
+        public int roadValue { get; set; }
+
+        public uint divisionMin { get; private set; } = 3;
+        public uint divisionRandMax { get; private set; } = 4;
+        public uint roomMinX { get; private set; } = 5;
+        public uint roomRandMaxX { get; private set; } = 2;
+        public uint roomMinY { get; private set; } = 5;
+        public uint roomRandMaxY { get; private set; } = 2;
+
+        // 消去 (clear) //
+        public TDerived ClearRoom() {
+            roomValue = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearWay() {
+            roadValue = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearRoad() {
+            roadValue = 0;
+            return (TDerived)this;
+        }
+
+        public TDerived ClearValue() {
+            ClearRoom();
+            ClearRoad();
+            return (TDerived)this;
+        }
+
+        // 代入 (setter returns own instance) //
+
+        public TDerived SetRoom(int roomValue) {
+            this.roomValue = roomValue;
+            return (TDerived)this;
+        }
+
+        public TDerived SetWay(int roadValue) {
+            this.roadValue = roadValue;
+            return (TDerived)this;
+        }
+
+        public TDerived SetRoad(int roadValue) {
+            this.roadValue = roadValue;
+            return (TDerived)this;
+        }
+
+        public TDerived SetRogueLike(uint divisionMin, uint divisionRandMax, uint roomMinX,
+            uint roomRandMaxX, uint roomMinY, uint roomRandMaxY) {
+            this.divisionMin = divisionMin;
+            this.divisionRandMax = divisionRandMax;
+            this.roomMinX = roomMinX;
+            this.roomRandMaxX = roomRandMaxX;
+            this.roomMinY = roomMinY;
+            this.roomRandMaxY = roomRandMaxY;
+            return (TDerived)this;
+        }
+
+        /* Constructors */
+        public RectBaseSimpleRogueLike() { } // = default();
+
+        public RectBaseSimpleRogueLike(int roomValue) {
+            this.roomValue = roomValue;
+        }
+
+        public RectBaseSimpleRogueLike(int roomValue, int roadValue) {
+            this.roomValue = roomValue;
+            this.roadValue = roadValue;
+        }
+
+        public RectBaseSimpleRogueLike(int roomValue, int roadValue, uint divisionMin,
+            uint divisionRandMax, uint roomMinX, uint roomRandMaxX, uint roomMinY, uint roomRandMaxY) {
+            this.roomValue = roomValue;
+            this.roadValue = roadValue;
+            this.divisionMin = divisionMin;
+            this.divisionRandMax = divisionRandMax;
+            this.roomMinX = roomMinX;
+            this.roomRandMaxX = roomRandMaxX;
+            this.roomMinY = roomMinY;
+            this.roomRandMaxY = roomRandMaxY;
+        }
+
+        public RectBaseSimpleRogueLike(MatrixRange matrixRange) {
+            this.startX = (uint)matrixRange.x;
+            this.startY = (uint)matrixRange.y;
+            this.width = (uint)matrixRange.w;
+            this.height = (uint)matrixRange.h;
+        }
+
+        public RectBaseSimpleRogueLike(MatrixRange matrixRange, int roomValue) : this(matrixRange) {
+            this.roomValue = roomValue;
+        }
+
+        public RectBaseSimpleRogueLike(MatrixRange matrixRange, int roomValue, int roadValue) : this(matrixRange, roomValue) {
+            this.roadValue = roadValue;
+        }
+
+        public RectBaseSimpleRogueLike(MatrixRange matrixRange, int roomValue, int roadValue, uint divisionMin,
+            uint divisionRandMax, uint roomMinX, uint roomRandMaxX, uint roomMinY, uint roomRandMaxY)
+            : this(matrixRange, roomValue, roadValue) {
+            this.roomValue = roomValue;
+            this.roadValue = roadValue;
+            this.divisionMin = divisionMin;
+            this.divisionRandMax = divisionRandMax;
+            this.roomMinX = roomMinX;
+            this.roomRandMaxX = roomRandMaxX;
+            this.roomMinY = roomMinY;
+            this.roomRandMaxY = roomRandMaxY;
+        }
+
+        // Vector2とかを引数に取るコンストラクタがあってもいいかもしれない
+        // 使い方を要検討.
+    }
+}
+
+

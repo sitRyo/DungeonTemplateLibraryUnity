02e917c39da08f27bf1ff73451e437b67563cdf6
sitRyo <al15030@shibaura-it.ac.jp>
2019-10-31 16:13:03 +0900

    [Add] Add Demo PerlinIsland DemoScene.

--- Assets/DungeonTemplateLibrary/Demo/PerlinIsland/GeneratePerlinIsland.cs
+++ Assets/DungeonTemplateLibrary/Demo/PerlinIsland/GeneratePerlinIsland.cs
@@ -13,20 +13,64 @@
 #######################################################################################*/
 
 using DTL.Range;
-using UnityEngine;
 using DTL.Shape;
+using UnityEngine;
+using System.Collections.Generic;
 
 public class GeneratePerlinIsland : MonoBehaviour {
-    private const int height = 48;
-    private const int width = 48;
+    public const int height = 200;
+    public const int width = 200;
 
     private PerlinIsland perlinIsland;
 
+    public List<GameObject> cubes = new List<GameObject>();
+
     private void Start () {
         var matrix = new int[height, width];
-        perlinIsland = new PerlinIsland(6.0, 8, 63);
+        perlinIsland = new PerlinIsland(6.0, 8, 150);
         perlinIsland.Draw(matrix);
         ConsoleDraw(matrix, height, width);
+        Generate(height, width, matrix, 150);
+    }
+
+    private void Generate(int height, int width, int[,] matrix, int maxHeight) {
+        int[] limited = new int[cubes.Count];
+        SetLimit(limited, maxHeight);
+        for (int row = 0; row < height; ++row) {
+            for (int col = 0; col < width; ++col) {
+                var cubeHeight = GetMapHeight(matrix[row, col], limited);
+                CubeInstantiate(row, cubeHeight, col);
+            }
+        }
+    }
+
+    private void CubeInstantiate(int x, int y, int z) {
+        Instantiate(cubes[y], new Vector3(x, y, z), Quaternion.identity);
+    }
+
+    private int GetMapHeight(int arrayValue, int[] limit) {
+        var length = limit.Length;
+        var value = length-1;
+        for (int i = 0; i < length; ++i) {
+            if (arrayValue < limit[i]) {
+                value = i-1;
+                return value;
+            }
+        }
+
+        return value;
+    }
+
+    private int[] SetLimit(int[] limit, int maxHeight) {
+        var length = limit.Length;
+        var heightUnit = (int) maxHeight / length;
+        var count = 0;
+        for (int i = 0; i < length; ++i) {
+            limit[i] = count;
+            count += heightUnit;
+        }
+
+        return limit;
     }
 
     private void ConsoleDraw(int[,] matrix, int h, int w) {

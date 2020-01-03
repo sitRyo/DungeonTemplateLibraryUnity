7d7bff9066e27dd6717af50aca9509f843dd5b79
sitRyo <al15030@shibaura-it.ac.jp>
2020-01-03 17:32:41 +0900

    [Refactoring] Refactoring RectBaseRogueLike

--- Assets/DungeonTemplateLibrary/Scripts/Range/RectBaseRogueLike.cs
+++ Assets/DungeonTemplateLibrary/Scripts/Range/RectBaseRogueLike.cs
@@ -32,28 +32,115 @@ namespace DTL.Range {
 
         /* Get Member Value */
 
-        public int outSideWall {
+        public int outsideWall {
             get => this.rogueLikeList.outsideWallId;
+            protected set;
         }
 
         public int insideWall {
             get => this.rogueLikeList.insideWallId;
+            protected set;
         }
 
         public int room {
             get => this.rogueLikeList.roomId;
+            protected set;
         }
 
         public int entrance {
             get => this.rogueLikeList.entranceId;
+            protected set;
         }
 
         public int way {
             get => this.rogueLikeList.wayId;
+            protected set;
         }
 
         public int wall {
             get => this.rogueLikeList.outsideWallId;
+            protected set;
+        }
+
+        /* for method chaining */
+
+        public TDerived GetOutsideWall(ref int value) {
+            value = this.outSideWall;
+            return (TDerived) this;
+        }
+
+        public TDerived GetinsideWall(ref int value) {
+            value = this.insideWall;
+            return (TDerived) this;
+        }
+
+        public TDerived GetRoom(ref int value) {
+            value = this.room;
+            return (TDerived) this;
+        }
+
+        public TDerived GetEntrance(ref int value) {
+            value = this.entrance;
+            return (TDerived) this;
+        }
+
+        public TDerived GetWay(ref int value) {
+            value = this.way;
+            return (TDerived) this;
+        }
+
+        public TDerived GetWall(ref int value) {
+            value = this.wall;
+            return (TDerived) this;
+        }
+
+        public TDerived GetMaxWay(ref int value) {
+            value = this.maxWay;
+            return (TDerived) this;
+        }
+
+        public RogueLikeList GetValue() {
+            return this.rogueLikeList;
+        }
+
+        public TDerived SetOutsideWall(ref int value) {
+            this.outSideWall = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetinsideWall(ref int value) {
+            this.insideWall = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetRoom(ref int value) {
+            this.room = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetEntrance(ref int value) {
+            this.entrance = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetWay(ref int value) {
+            this.way = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetWall(ref int value) {
+            this.wall = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetMaxWay(ref int value) {
+            this.maxWay = value;
+            return (TDerived) this;
+        }
+
+        public TDerived SetRogueLikeList(RogueLikeList rogueLikeList) {
+            rogueLikeList = this.rogueLikeList;
+            return (TDerived) this;
         }
 
         /* Clear */
@@ -92,6 +179,12 @@ namespace DTL.Range {
             this.rogueLikeList = new RogueLikeList();
             ClearMaxWay();
             return (TDerived)this;
+        }
+
+        public TDerived Clear() {
+            this.ClearRange();
+            this.ClearValue();
+            return (TDerived) this;
         }
 
         /* Constructors */
